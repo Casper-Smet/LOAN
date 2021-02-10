283c46524cc1d32a55f462984c04cc100a2002b6
Casper Smet <casper.smet@student.hu.nl>
2021-02-10 18:14:37 +0100

    Linting, moved reporters to model functions

--- loan/batchrunner.py
+++ loan/batchrunner.py
@@ -3,14 +3,14 @@ from pathlib import Path
 from mesa.batchrunner import BatchRunner, BatchRunnerMP
 
 from helpers import graph_from_json
-from model import HumanModel, get_hitpoints, get_ill_vertices, get_end
+from model import HumanModel
 
 
 def run_batch(iterations=50, max_steps=100, use_mp: bool = True):
     graph = graph_from_json(Path("./loan/network.json"))
     fixed_params = {"N": 1, "network": graph}
     variable_params = {"illness_chance": [0.1, 0.2, 0.3, 0.4]
-    }
+                       }
 
     runner = BatchRunnerMP if use_mp else BatchRunner
 
@@ -20,11 +20,13 @@ def run_batch(iterations=50, max_steps=100, use_mp: bool = True):
         fixed_params,
         iterations=iterations,  # Iterations per combination of parameters
         max_steps=max_steps,
-        model_reporters={"Hitpoints": get_hitpoints, "Ill vertices": get_ill_vertices, "End time": get_end}
-        )
+        model_reporters={"Hitpoints": HumanModel.get_hitpoints,
+                         "Ill vertices": HumanModel.get_ill_vertices, "End time": HumanModel.get_end}
+    )
 
     batch_run.run_all()  # Run all simulations
-    run_data = batch_run.get_model_vars_dataframe()  # Get DataFrame with collected data
+    # Get DataFrame with collected data
+    run_data = batch_run.get_model_vars_dataframe()
     return run_data
 
 
