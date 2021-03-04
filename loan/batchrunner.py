c69adcda0bb9fb74ade38a71ee88cafcbb2709d7
Casper Smet <casper.smet@student.hu.nl>
2021-03-04 18:02:16 +0100

    Removed illness chance as variable param

--- loan/batchrunner.py
+++ loan/batchrunner.py
@@ -9,8 +9,7 @@ from loan.model import HumanModel
 def run_batch(iterations=100, max_steps=100, use_mp: bool = True, network_path: str = "./loan/data/network.json"):
     graph = graph_from_json(Path(network_path))
     fixed_params = {"N": 1, "network": graph}
-    variable_params = {"illness_chance": (0.2, 0.3, 0.4),
-                       "factory_location": tuple(range(1, 15))}
+    variable_params = {"factory_location": tuple(range(1, 15))}
 
     runner = BatchRunnerMP if use_mp else BatchRunner
     
