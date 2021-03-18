55044577816aee5c24221dba48715a2c319d653b
Denbergvanthijs <denbergvanthijs@gmail.com>
2021-03-18 16:30:05 +0100

    Fix batchrunner by cloning mesa repo

--- loan/batchrunner.py
+++ loan/batchrunner.py
@@ -1,28 +1,29 @@
-from pathlib import Path
+from mesa.batchrunner import BatchRunnerMP
 
-from mesa.batchrunner import BatchRunner
-
-from loan.helpers import graph_from_json
 from loan.model import HumanModel
 from collections import OrderedDict
 
-def run_batch(iterations=1_0_0, max_steps=1_0_0, network_path: str = "./loan/data/network.json"):
-    graph = graph_from_json(Path(network_path))
-    fixed_params = {}
-    variable_params = {"factory_location": tuple(range(1, 15)),
-                                   "N": (1, 2, 4, 8, 10),
-                                   "helper_type": ("helperagent", "greedyhelperagent")}
 
-    batch_run = BatchRunner(HumanModel,
-                            variable_parameters=variable_params,
-                            fixed_parameters=fixed_params,
-                            iterations=iterations,  # Iterations per combination of parameters
-                            max_steps=max_steps,
-                            model_reporters={"Hitpoints": HumanModel.get_hitpoints,
-                                             "Ill vertices": HumanModel.get_ill_vertices,
-                                             "End time": HumanModel.get_end,
-                                             "Vertices healed": HumanModel.get_healed_count})
+def run_batch(iterations=10, max_steps=100):
+    fixed_params = {}
+
+    variable_params = OrderedDict()
+    variable_params["factory_location"] = tuple(range(1, 15))
+    variable_params["N"] = (1, 2, 4, 8, 1)
+    variable_params["helper_type"] = ("helperagent", "greedyhelperagent")
+
+    model_reporters = {"Hitpoints": HumanModel.get_hitpoints,
+                       "Ill vertices": HumanModel.get_ill_vertices,
+                       "End time": HumanModel.get_end,
+                       "Vertices healed": HumanModel.get_healed_count}
+
+    batch_run = BatchRunnerMP(HumanModel,
+                              nr_processes=None,
+                              variable_parameters=variable_params,
+                              fixed_parameters=fixed_params,
+                              iterations=iterations,  # Iterations per combination of parameters
+                              max_steps=max_steps,
+                              model_reporters=model_reporters)
 
     batch_run.run_all()  # Run all simulations
-    run_data = batch_run.get_model_vars_dataframe()  # Get DataFrame with collected data
-    return run_data
+    return batch_run.get_model_vars_dataframe()  # Get DataFrame with collected data
